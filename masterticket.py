8b5aa2481e2f27313ebd2636be48137bf679914b
Mason Aviles <masonaviles@Masons-MacBook-Pro.local>
2021-04-22 22:40:57 -0700

    refactor and add some error handling

--- masterticket.py
+++ masterticket.py
@@ -1,13 +1,17 @@
+SERVICE_CHARGE = 2
 TICKET_PRICE = 10
 
 tickets_remaining = 100
+
+# calc_price function
+def calc_price(number_of_tickets):
+  return (number_of_tickets * TICKET_PRICE) + SERVICE_CHARGE
 
 
 # Run this code continuously until we run out of tickets
 while tickets_remaining >= 1:
     
     # Output how many tickets are remaining using the tickets_remaining variable
-    
     print("There are {} tickets remaining.".format(tickets_remaining))
     
     # Gather the user's name and assign it to a new variable
@@ -15,26 +19,33 @@ while tickets_remaining >= 1:
     
     # Prompt the user by name and ask how many tickets they would like
     num_tickets = input("How many tickets would you like, {}?  ".format(name))
-    num_tickets = int(num_tickets)
-    # Calculate the price (number of tickets multiplied by the price) and assign it to a variable
-    amount_due = num_tickets * TICKET_PRICE
-    # Output the price to the screen
-    print("The total due is ${}".format(amount_due))
-    
-    # Prompt user if they want to proceed.  Y/N?
-    should_proceed = input("Do you want to proceed?  Y/N  ")
-    
-    # If they want to proceed
-    if should_proceed.lower() == "y":
-        # print out to the screen "SOLD!" to confirm purchase
-        # TODO: Gather credit card information and process it.
-        print("SOLD!")
-        # and then decrement the tickets remaining by the number of tickets purchased
-        tickets_remaining -= num_tickets
-    # Otherwise....
+
+    try:
+      num_tickets = int(num_tickets)
+      if num_tickets > tickets_remaining:
+        raise ValueError("There are only {} tickets remaining".format(tickets_remaining))
+    except ValueError as err:
+      print("Oh no, we ran into an issue. {}. Please try again.".format(error))
     else:
-        # Thank them by name
-        print("Thank you anyways, {}!".format(name))
+      # Calculate the price (number of tickets multiplied by the price) and assign it to a variable
+      amount_due = calc_price(num_tickets)
+      # Output the price to the screen
+      print("The total due is ${}".format(amount_due))
+    
+      # Prompt user if they want to proceed.  Y/N?
+      should_proceed = input("Do you want to proceed?  Y/N  ")
+    
+      # If they want to proceed
+      if should_proceed.lower() == "y":
+          # print out to the screen "SOLD!" to confirm purchase
+          # TODO: Gather credit card information and process it.
+          print("SOLD!")
+          # and then decrement the tickets remaining by the number of tickets purchased
+          tickets_remaining -= num_tickets
+      # Otherwise....
+      else:
+          # Thank them by name
+          print("Thank you anyways, {}!".format(name))
 
 # Notify user that the tickets are sold out    
 print("Sorry the tickets are all sold out!!! :(")
